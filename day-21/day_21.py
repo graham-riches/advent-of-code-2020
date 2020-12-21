99317d804afe38cc06193b03bc24eef40a4a865f
Graham Riches <graham@attabotics.com>
2020-12-21 07:36:27 -0700

    added a few comments for part two since it's a bit convoluted

--- day-21/day_21.py
+++ day-21/day_21.py
@@ -32,14 +32,22 @@ if __name__ == '__main__':
     allergen_type = dict()
     allergens_set = set()
     while len(allergens_set) != len(allergens):
-        # get a sorted list of allergens by the length of possible types
+        # get a sorted list of allergens by the length of possible types. The first element should always have length
+        # of one, which corresponds to a match to a specific ingredient type
         sorted_allergens = sorted(allergen_dict.items(), key=lambda item: len(item[1]))
         item = sorted_allergens[0]
+
+        # get the logic not of the current allergens the existing set
         new_allergen = {s for s in item[1] if s not in allergens_set}
+
+        # update the existing set with the new value
         allergens_set = set.union(allergens_set, new_allergen)
+
+        # stuff the ingredient/allergen combo into the new dictionary
         allergen_strs = [str(allergen) for allergen in new_allergen]
         allergen_type[allergen_strs[0]] = item[0]
-        # remove the current allergen from every other entry
+
+        # remove the current allergen from every other entry so that the list can be re-sorted
         new_dict = dict()
         for other in sorted_allergens:
             if other != item:
