e9a0836af8997fad6262b019d9f30e909de341e0
Graham Riches <graham@attabotics.com>
2020-12-23 11:23:48 -0700

    day 23 solution parts one and two in C++. Full solution runs in < 10 seconds which is not terrible considering it's 10 million iterations :D

--- day-23/circular_list.h
+++ day-23/circular_list.h
@@ -11,37 +11,139 @@
 
 /********************************** Includes *******************************************/
 #include <algorithm>
+#include <array>
+#include <iostream>
 #include <memory>
 #include <numeric>
+#include <unordered_map>
 #include <vector>
 
+/********************************** Types *******************************************/
+/**
+ * @brief single hashed list node element
+ * @tparam T template type parameter
+*/
 template <typename T>
 struct Node {
     T data;
     Node *next;
 };
 
-
+/**
+ * @brief circular linked list hashed data structure. This provides fast access,
+ *        fast insertion, and fast deletion everywhere in the list, and also maintains
+ *        the circular ordering of elements. It is obviously less than memory efficient.
+ * @note  as this data structure is designed purely with the AoC 2020 day 23 solution in mind,
+ *        there are a few caveats that make this not quite as general as it could be (but more efficient
+ *        for the task at hand):
+ *          1. It is implicitly assumed that the list only contains unique items
+ *          2. the maximum size of the container is constant. Nodes can only be added if they are first
+ *             removed. This basically means it is just a re-ordering
+ *          
+ * @tparam T templated type for the container (not strictly required, but good practice anyhow)
+*/
 template <typename T>
 class CircularList {
   public:
-    CircularList( std::vector<T> data ) {
-        /* create the linked list */
-        size = data.size( );
-        nodes = std::make_unique<Node<T>[]>( size );
-        nodes[0].next = &nodes[1];
-        for ( size_t i = 0; i < size - 1; i++ ) {
-            nodes[i].data = data[i];
-            nodes[i].next = &nodes[i + 1LL];
-        }
-        nodes[size].next = &nodes[0];
+    /**
+     * @brief create a new circular hashed list container
+     * @param data vector of input data to store in the list
+    */
+    CircularList( std::vector<T> data )
+        : nodes( data.size( ) )
+        , size( data.size( ) )
+        , minimum( *std::min_element( data.cbegin( ), data.cend( ) ) )
+        , maximum( *std::max_element( data.cbegin( ), data.cend( ) ) ) {
 
-        /* set the start node */
-        active_node = &nodes[0];
+        /* setup the circular linked list */
+        for ( size_t i = 0; i < size; i++ ) {
+            nodes[i].data = data[i];
+            nodes[i].next = &nodes[( i + 1LL ) % size];
+        }
+
+        /* initialize the hash table */
+        for ( size_t i = 0; i < size; i++ ) {
+            hash_table[nodes[i].data] = &nodes[i];
+        }
+    }
+
+    /**
+     * @brief remove a set of nodes after a specific element in the list
+     * @param element the element to remove nodes after
+     * @param count how many elements to pop
+     * @return vector of popped nodes
+    */
+    std::vector<T> remove_after_element( T element, int count ) {
+        Node<T> *node = hash_table[element];
+        std::vector<T> popped_entries( count );
+        for ( int i = 0; i < count; i++ ) {
+            popped_entries[i] = node->next->data;
+            node->next = node->next->next;
+        }
+        return popped_entries;
+    }
+
+    /**
+     * @brief insert a sequences of nodes after a particular element in the collection
+     * @param element the element to insert after
+     * @param nodes list of nodes
+    */
+    void insert_after_element( T element, std::vector<T> entries ) {
+        Node<T> *node = hash_table[element];
+        T final_element = node->next->data;
+        for ( auto &entry : entries ) {
+            node->next = hash_table[entry];
+            node = node->next;
+        }
+        node->next = hash_table[final_element];
+    }
+
+    /**
+     * @brief get the next element from an element
+     * @param element the element value
+     * @return next element
+    */
+    T get_next( T element ) {
+        Node<T> *node = hash_table[element];
+        return node->next->data;
+    }
+
+    /**
+     * @brief getter to get the min item in the list     
+    */
+    T get_minimum( void ) {
+        return minimum;
+    }
+
+    /**
+     * @brief getter to get the max item in the list     
+    */
+    T get_maximum( void ) {
+        return maximum;
+    }
+
+    /**
+     * @brief debug function to print the list ordering from an element
+     * @param element the element to print from
+    */
+    void print_from_element( T element ) {
+        Node<T> *node = hash_table[element];
+        T start_value = node->data;
+        std::cout << start_value << " ";
+
+        T data = node->next->data;
+        while ( data != start_value ) {
+            std::cout << data << " ";
+            node = node->next;
+            data = node->next->data;
+        }
+        std::cout << "\n";
     }
 
   private:
-    std::unique_ptr<Node<T>[]> nodes;
-    Node<T> *active_node;
+    std::unordered_map<T, Node<T> *> hash_table;
+    std::vector<Node<T>> nodes;
     size_t size;
+    T maximum;
+    T minimum;
 };
--- day-23/day_23.cpp
+++ day-23/day_23.cpp
@@ -12,47 +12,98 @@
 #include <algorithm>
 #include <chrono>
 #include <cstdint>
-#include <fstream>
-#include <iostream>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <set>
-#include <sstream>
-#include <vector>
-#include <list>
 
 /********************************** Functions *******************************************/
 
-std::list<int> get_next_entries( std::list<int> &list, int current_id, int count ) {
-    auto it = std::find(list.begin(), list.end(), current_id);
-    auto index = std::next(it, count);
-    std::list<int> entries(count);
-    /*
-    if ( index + count > list.size() ) {
-        
-    }
+class CrabbyCupGame {
+  public:
+    CrabbyCupGame( CircularList<int> list )
+        : list( list )
+        , min_value( list.get_minimum( ) )
+        , max_value( list.get_maximum( ) ){ };
+
+    /**
+     * @brief play the game for a set number of rounds
+     * @param rounds how many rounds to play
+     * @param starting_cup cup to start with
+     * @return the circular list object
     */
-    
+    CircularList<int> play_game( int rounds, int starting_cup ){
+        int active_cup = starting_cup;
+        while (rounds--){
+            active_cup = play_round(active_cup);            
+        }
+        return list;
+    }
 
+  private:
+    CircularList<int> list;
+    int min_value;
+    int max_value;
 
+    /**
+    * @brief get the destination cup value
+    * @param round_cups the current cups in play
+    * @param active cup the currently active main cup
+    * @return int id
+    */
+    int get_destination_cup( std::vector<int> round_cups, int active_cup ) {
+        int target_id = active_cup - 1;
 
-    //list.erase(index, index + count);
-    //return entries;
-    return { };
-}
+        while ( true ) {
+            target_id = ( target_id < min_value ) ? max_value : target_id;
+            auto count = std::count( round_cups.cbegin( ), round_cups.cend( ), target_id );
+            if ( count == 0 ) {
+                break;
+            } else {
+                target_id--;
+            }
+        }
+        return target_id;
+    }
 
-
+    /**
+    * @brief play a round
+    * @param active_cup the active cup for the round
+    * @return next cup
+    */
+    int play_round( int active_cup ) {
+        auto cups = list.remove_after_element( active_cup, 3 );
+        int destination = get_destination_cup( cups, active_cup );
+        list.insert_after_element(destination, cups);        
+        return list.get_next(active_cup);
+    }
+};
 
 /**
  * @brief main application entry point
 */
 int main( int64_t argc, char *argv[] ) {
-    std::vector<int> input{3, 8, 9, 1, 2, 5, 4, 6, 7};
-    //auto test = get_next_entries( input, 8, 3 );
-    
-    CircularList test{input};
+    auto start = std::chrono::steady_clock::now();
 
+    /*------------------------------ Part One Solution ------------------------------*/
+    std::vector<int> input{ 5, 9, 8, 1, 6, 2, 7, 3, 4 };
+    CircularList list{ input };
+    CrabbyCupGame game_one{ list };
+    auto result = game_one.play_game(100, input[0]);
+    result.print_from_element(1);
+
+    /*------------------------------ Part Two Solution ------------------------------*/
+    std::vector<int> more_cups(1000000 - input.size());
+    std::iota(more_cups.begin(), more_cups.end(), *std::max_element(input.begin(), input.end()) + 1 );
+    input.insert(input.end(), more_cups.begin(), more_cups.end());
+    CircularList new_list{input};
+    CrabbyCupGame game_two{new_list};
+    auto round_two_result = game_two.play_game( 10000000, input[0] );
+    auto multiple_one = round_two_result.get_next(1);
+    auto multiple_two = round_two_result.get_next(multiple_one);
+
+    int64_t multiple = static_cast<int64_t>(multiple_one) * static_cast<int64_t>(multiple_two);
+    std::cout << "Multiple is: " << multiple << "\n";
+    
+    auto end = std::chrono::steady_clock::now();
+
+    std::cout << "Elapsed Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " milliseconds \n";
 
     return 0;
 }
